7aafc6df17ab18351f29222856b95120dab1e026
Amitava Haldar <onno@iMacHuiskamer.local>
2020-12-01 16:09:44 +0100

    refactor

--- tripple-generator/generate-medewerker-tripples.ts
+++ tripple-generator/generate-medewerker-tripples.ts
@@ -4,22 +4,53 @@
  * 
  * KIK-V demo data generator for demo tripels
  * 
- * Inhuurovereenkomst -> ZZPer -> Personeel niet in dienst
+ * 
+ * Soort overeenkomst                                  Bijbehorende rol            Groep
+ * ------------------------------------------------------------------------------------------------------------
+ * 
+ * Werkovereenkomst                                    Medewerker                  -
+ * │                                                   │
+ * ├── Inhuurovereenkomst                              ├── ZZPer                   Personeel niet in loondienst
+ * │                                                   │
+ * ├── Arbeidsovereenkomst                             ├── Werknemer               Personeel in loondienst                        
+ * │   ├── Arbeidsovereenkomst bepaalde tijd           │   │
+ * │   │   └── Arbeidsovereenkomst BBL                 │   └── Leerling-Werknemer
+ * │   └── Arbeidsovereenkomst onbepaalde tijd         │
+ * │                                                   │
+ * ├── Oproepovereenkomst                              ├── Werknemer               Personeel in loondienst
+ * │   ├── Min-max contract                            │
+ * │   ├── Nuluren contract                            │
+ * │   └── Oproepovereenkomst met voorovereenkomst     │
+ * │                                                   │
+ * ├── Stageovereenkomst                               ├── Stagiair                -
+ * │                                                   │
+ * ├── Uitzendovereenkomst                             Medewerker                  Personeel niet in loondienst
+ * │   └── Payrollovereenkomst                         │
+ * │                                                   │
+ * └── Vrijwilligersovereenkomst                       └── Vrijwilliger            -
  * 
  */
+ 
 import mocker from 'mocker-data-generator';
 import { inspect } from 'util';
 
 export function generateMedewerkerTripples(): any {
-    const kikPersoneel = {
+    const medewerker = {
         nodeId: {
             faker: 'random.uuid'
         },
-        personeelType: {
+        groep: {
             values: [
                 'kik:PersoneelInLoondienst',
-                'kik:PersoneelNietInLoondienst'
+                'kik:PersoneelNietInLoondienst',
+                '-'
             ]
+        },
+        'object.groep=="kik:PersoneelInLoondienst",werkovereenkomst': {
+            values: ['']
+        },
+        'object.groep=="kik:PersoneelNietInLoondienst",speed': {
+            faker: 'random.number'
         }
     };
 
