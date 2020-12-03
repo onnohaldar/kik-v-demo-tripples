ceda4204f683a598cab4104706b1a05dc6e75637
Amitava Haldar <onno.haldar@icloud.com>
2020-12-03 15:37:20 +0100

    refactor

--- tripple-generator/generate-kik-v-demo-tripples.ts
+++ tripple-generator/generate-kik-v-demo-tripples.ts
@@ -10,27 +10,22 @@ import { inspect } from 'util';
 import { argv } from 'process';
 //import { NamespaceManagerInstance, BlankNode, IRI, TypedLiteral, LangLiteral, NTriple, NQuad  } from 'rdflib-ts';
 
-import { generateKikVMedewerkerData, KikVMedewerkerData } from './generate-kik-v-medewerker-data';
+import { generateKikVMedewerkerData } from './generate-kik-v-medewerker-data';
+import { generateKikVOvereenkomstData, KikVOvereenkomstData } from './generate-kik-v-overeenkomst-data';
 
 interface Options {
     medewerkersToGenerate: number;
 }
 
-interface KikVDemoData {
-    medewerkers: KikVMedewerkerData[];
-}
-
 console.log(argv[2]);
 const options: Options = JSON.parse(argv[2]);
 
-let generatedData: KikVDemoData = {
-    medewerkers: generateKikVMedewerkerData({ numberToGenerate: options.medewerkersToGenerate })
-}
-
-
-
+let medewerkers =  generateKikVMedewerkerData({ numberToGenerate: options.medewerkersToGenerate });
 console.log('======================================================================');
-console.log(inspect(generatedData, { depth: 10 }));
+console.log(inspect(medewerkers, { depth: 10 }));
 console.log('======================================================================');
 
-
+const overeenkomsten = generateKikVOvereenkomstData(medewerkers, { minToGenerate: 1, maxToGenerate: 3, createDatePastInYears: 2 });
+console.log('======================================================================');
+console.log(inspect(overeenkomsten, { depth: 10 }));
+console.log('======================================================================');
