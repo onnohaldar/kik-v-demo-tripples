4ac07951438b1518760b42f8c0fdad2357b03213
Amitava Haldar <onno.haldar@icloud.com>
2020-12-03 13:07:26 +0100

    refactor

--- tripple-generator/generate-kik-v-demo-tripples.ts
+++ tripple-generator/generate-kik-v-demo-tripples.ts
@@ -7,16 +7,24 @@
  */
 
 import { inspect } from 'util';
+import { argv } from 'process';
 //import { NamespaceManagerInstance, BlankNode, IRI, TypedLiteral, LangLiteral, NTriple, NQuad  } from 'rdflib-ts';
 
 import { generateKikVMedewerkerData, KikVMedewerkerData } from './generate-kik-v-medewerker-data';
+
+interface Options {
+    medewerkersToGenerate: number;
+}
 
 interface KikVDemoData {
     medewerkers: KikVMedewerkerData[];
 }
 
+console.log(argv[2]);
+const options: Options = JSON.parse(argv[2]);
+
 let generatedData: KikVDemoData = {
-    medewerkers: generateKikVMedewerkerData({ numberToGenerate: 10 })
+    medewerkers: generateKikVMedewerkerData({ numberToGenerate: options.medewerkersToGenerate })
 }
 
 console.log('======================================================================');
