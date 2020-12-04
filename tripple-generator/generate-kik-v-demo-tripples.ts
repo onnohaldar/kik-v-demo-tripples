3ecaa49e30ebc9469188414d1f221a99f3cdbc7b
Amitava Haldar <onno@iMacHuiskamer.local>
2020-12-04 07:40:32 +0100

    refactor

--- tripple-generator/generate-kik-v-demo-tripples.ts
+++ tripple-generator/generate-kik-v-demo-tripples.ts
@@ -6,8 +6,7 @@
  * 
  */
 
-import { writeFileSync, readFileSync } from 'fs';
-import { join, resolve } from 'path';
+
 import { inspect } from 'util';
 import { argv } from 'process';
 //import { NamespaceManagerInstance, BlankNode, IRI, TypedLiteral, LangLiteral, NTriple, NQuad  } from 'rdflib-ts';
@@ -25,11 +24,4 @@ const options: GeneratorOptions = JSON.parse(argv[2]);
 let medewerkers =  generateKikVMedewerkerData({ numberToGenerate: options.medewerkersToGenerate });
 
 const overeenkomsten = generateKikVOvereenkomstData(medewerkers, { minToGenerate: 1, maxToGenerate: 3, createDatePastInYears: 2 });
-
-
-const turtleTemplateFile = join(__dirname, 'kik-v-demo-tripples.template.ttl');
-const turtleTemplate = readFileSync(turtleTemplateFile, 'utf-8');
-
-const destPath = resolve(__dirname, '..', 'generated-tripples');
-const turtleFile = join(destPath, 'kik-v-demo-tripples-v01.ttl');
 
---
+++ tripple-generator/write-kik-v-tripples-file.ts
@@ -0,0 +1,24 @@
+/**
+ * 
+ * File: write-kik-v-tripples-file.ts
+ * 
+ * KIK-V demo data generator
+ * 
+ */
+
+import { writeFileSync, readFileSync } from 'fs';
+import { join, resolve } from 'path';
+
+import { KikVMedewerkerData } from './generate-kik-v-medewerker-data';
+
+export function writeKikVTripplesFile({
+
+}) {
+    const turtleTemplateFile = join(__dirname, 'kik-v-demo-tripples.template.ttl');
+    const turtleTemplate = readFileSync(turtleTemplateFile, 'utf-8');
+    
+    const destPath = resolve(__dirname, '..', 'generated-tripples');
+    const turtleFile = join(destPath, 'kik-v-demo-tripples-v01.ttl');
+    
+        
+}
